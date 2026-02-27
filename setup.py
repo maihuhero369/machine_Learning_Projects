ea392d4b86941e6a6af6d2a8948d3a48acda1d20
maihuhero369 <mai.hu.hero.369@gmail.com>
2026-02-28 01:49:38 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+from typing import List
+
+setup(
+    name="sensor",
+    version="0.1",
+    author="Hero",
+    author_email="<maiHuHero369@gmail.com>",
+    packages=find_packages(),
+
+)
